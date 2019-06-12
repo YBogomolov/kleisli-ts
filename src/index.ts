35c6fafb7fed0c07d0fd1c53ea2eca3e6e344acf
Yuriy Bogomolov <cronus.cpp@gmail.com>
2019-06-12 15:49:26 +0300

    Change license to Apache 2.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 // tslint:disable:max-line-length
 /*
+ *  Copyright 2019 Yuriy Bogomolov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  * TypeScript port of John A. De Goes's talk about KleisliIO at LambdaConf'18:
  * https://www.youtube.com/watch?v=L8AEj6IRNEE
  * with additional instances of Applicative, Bifunctor, etc.
